0163c89aece12733fde8a5ab8ba6e05f53ff3a96
Petr Smirnov <psmirnov2000@gmail.com>
2023-09-10 00:42:18 +0200

    adding localization for german, work in progress

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,10 @@ module Wedding
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.i18n.available_locales = %i[en fr]
+    config.i18n.available_locales = %i[en de]
     config.i18n.default_locale = :en
+
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
